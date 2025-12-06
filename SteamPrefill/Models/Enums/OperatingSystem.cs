bbe526019783d6b4897ac2a8a93a19a83d6cc4e2
Tim Pilius <tpilius@gmail.com>
2025-12-06 17:28:17 -0500

    Adding Android to the OperatingSystem enum.  Looks like Valve is adding android support at some point in the future.

--- SteamPrefill/Models/Enums/OperatingSystem.cs
+++ SteamPrefill/Models/Enums/OperatingSystem.cs
@@ -3,6 +3,9 @@
     [Intellenum(typeof(string))]
     public sealed partial class OperatingSystem
     {
+        // Looks like there is currently one game that is android : Walkabout Mini Golf
+        public static readonly OperatingSystem Android = new("android");
+
         public static readonly OperatingSystem Windows = new("windows");
         public static readonly OperatingSystem MacOS = new("macos");
         public static readonly OperatingSystem Linux = new("linux");
